b97a541c1cba1163b92d63af01a29eb292120613
Mai Tung <goingdownfr@gmail.com>
2021-02-27 22:17:53 +0700

    rm logs

--- layouts/auth.tsx
+++ layouts/auth.tsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Home.module.css";
 import React from "react";
 import { classNamesParser } from "utils/classNamesParser";
-console.log(classNamesParser(["main", "auth-main"], styles))
+
 const AuthLayout = ({ children }) => (
   <main className={classNamesParser(["main", "auth-main"], styles)}>
     {children}
