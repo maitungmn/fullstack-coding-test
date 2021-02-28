89d50df07008c7b278f2cd409e901410ac21e08e
Mai Tung <goingdownfr@gmail.com>
2021-02-28 12:02:20 +0700

    rm logs

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -9,30 +9,18 @@ import { auth } from "@fb/launcher";
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
     const cookies = nookies.get(ctx);
-    console.log(JSON.stringify(cookies, null, 2));
     const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
     const { uid, email } = token;
-
-    // the user is authenticated!
-    // FETCH STUFF HERE
 
     return {
       props: { message: `Your email is ${email} and your UID is ${uid}.` },
     };
   } catch (err) {
-    // either the `token` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
-    // either the `token` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
     return {
       redirect: {
         permanent: false,
         destination: "/auth/sign-in",
       },
-      // `as never` is required for correct type inference
-      // by InferGetServerSidePropsType below
       props: {} as never,
     };
   }
