98ca57a80989fe723cf68fb09d520be85508982f
Mai Tung <goingdownfr@gmail.com>
2021-03-02 18:23:02 +0700

    add cors header

--- api/build-client.ts
+++ api/build-client.ts
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export const buildClient = (headers) => axios.create({
   baseURL: process.env.NEXT_PUBLIC_LAMBDA_ENDPOINT,
-  headers,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+    ...headers,
+  },
 })
